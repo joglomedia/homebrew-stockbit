4c0fd42fbb7e91f13115616f261642efc2be1eea
Edi Septriyanto <me@masedi.net>
2025-11-07 18:20:51 +0700

    first commit

---
+++ Cask/stockbit.rb
@@ -0,0 +1,25 @@
+cask "stockbit" do
+  version "1.43.1"
+  sha256 "8e1ac8850e5ad323e930cc7d3a0f0dfdbce46562a50876139a47c501fec46dbb"
+
+  url "https://sda-release-v2.stockbit.com/release/#{version}/macos/Stockbit.dmg",
+      verified: "sda-release-v2.stockbit.com/release/"
+  name "Stockbit"
+  desc "Stockbit adalah aplikasi untuk kamu yang ingin berdiskusi, analisa dan investasi saham dalam satu tempat."
+  homepage "https://stockbit.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  auto_updates true
+  depends_on macos: ">= :montery"
+
+  app "Stockbit.app"
+
+  zap trash: [
+    "~/Library/Application Support/Stockbit",
+    "~/Library/Saved Application State/com.stockbit.desktop.savedState",
+  ]
+end
